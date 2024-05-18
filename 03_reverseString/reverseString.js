2d3c04e469c906f4a01c945c799379fbdec101ed
ndthuy112 <ndthuy112@gmail.com>
2024-05-18 21:32:10 +0700

    01-07

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    output = ""
+    for (let letter of string) {
+        output = `${letter}${output}`
+    }
+    return output
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+/*const removeFromArray = function() {
+    let output = Array.from(arguments[0])
+    for (let i = 1; i < arguments.length; i++) {
+        output = output.filter((x) => (x !== arguments[i]))
+    }
+    return output;
+};*/
 
-};
+const removeFromArray = function(array,...args) {
+    for (let arg of args) {
+        array = array.filter((x) => (x !== arg))
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
